37f0dd578c16a692857098b10211a3c28dd0289d
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2024-07-12 22:34:48 +0530

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //header file
+int main() //main func. starts here
+{
+    printf("Hello World"); //prints this msg. on output screen
+    return 0; //does not return any integer value
+}
