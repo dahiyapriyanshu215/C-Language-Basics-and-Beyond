9c7a6a341740aee57471c69bffffd32539df7b4b
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2024-07-25 21:26:56 +0530

    Create String_Concatenation.c

---
+++ String_Concatenation.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+# include<string.h>
+
+/* WAP to concatenate two strings by using In-Built functions. */
+
+void main()
+{
+    char org1[20]="BEFORE";
+    char org2[20]="AFTER";
+
+    strcat(org2,org1);
+
+    puts(org2);
+}
