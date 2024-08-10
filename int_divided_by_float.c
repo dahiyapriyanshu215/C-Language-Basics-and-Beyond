947af7a30a561ce9e64f5c1f8478ff8bb8667d40
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2024-08-10 15:56:21 +0530

    Create Int_divided_by_float.c

---
+++ int_divided_by_float.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+// Program to show division of integer number with a float number.
+
+int main()
+{
+    int a;
+    printf("Enter an Integer Value : ");
+    scanf("%d",&a);
+    
+    float b;
+    printf("Enter a Float Value : ");
+    scanf("%f",&b);
+    
+    printf("Division of INT value by Float value is : %f", a/b);
+    
+    return 0;
+}
