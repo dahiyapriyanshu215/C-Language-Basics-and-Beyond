b66ece7fbcc260d092a8786493a37829c2896bf8
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2024-08-17 20:49:36 +0530

    Create Inverted_pyramid_of_stars.c

---
+++ Inverted_pyramid_of_stars.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+void main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows : ");
+    scanf("%d",&rows);
+
+    for(i=1;i<=rows;i++)
+    {
+        for(j=rows;j>=i;j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
