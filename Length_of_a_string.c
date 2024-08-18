f2591dd68a8412743fc0ac73ef556a728c331997
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2024-08-18 22:05:08 +0530

    Create Length_of_a_string.c

---
+++ Length_of_a_string.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+# include<string.h>
+
+/* WAP to compute length of a string by using In-Built functions. */
+
+void main()
+{
+    char list[] = "Welcome to Programming World.";
+
+    int len = strlen(list);
+
+    printf("The length of the string is : %d",len);
+}
