80eda262bada3293e780a9773fb956e56d293c27
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2024-09-12 21:00:15 +0530

    Create Reverse_a_string.c

---
+++ Reverse_a_string.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+# include<string.h>
+
+/* WAP to reverse a string by using In-Built functions. */
+
+void main()
+{
+
+    char list[15]="PROGRAMMING";
+
+    printf("Original String is : %s \n",list);
+
+    printf("Reversed String is : %s \n",strrev(list));
+}
